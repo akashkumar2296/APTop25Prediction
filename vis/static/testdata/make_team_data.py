d76583fb53469f858d8b19b0433eca7d251c1ede
mokskm <moksk@live.com>
2018-11-26 23:31:42 -0800

    team prediction

--- vis/static/testdata/make_team_data.py
+++ vis/static/testdata/make_team_data.py
@@ -7,12 +7,13 @@ with open("../../../model/Dataset_Final_Historical_data.csv", newline="") as csv
     hisdata = list(csv.reader(csvfile))
 with open("../../../model/Dataset_Final_Completed_Game_Data.csv", newline="") as csvfile:
     cpdata = list(csv.reader(csvfile))
-with open("../../../model/Dataset_Final_InProgress_Game_Data.csv", newline="") as csvfile:
+with open("../../../model/Dataset_Final_InProgress_Game_Data_Week_13.csv", newline="") as csvfile:
     ipdata = list(csv.reader(csvfile))
-with open("../../../model/Dataset_wk13_InProgress_ByQuarter_Game_Data.csv", newline="") as csvfile:
-    wk13ipdata = list(csv.reader(csvfile))
+with open("../../../model/Dataset_Final_Completed_Game_Data_Week_12.csv", newline="") as csvfile:
+    wk12cpdata = list(csv.reader(csvfile))
 
 
+#combining the historical data and the completed game data to generate the full set of team history up to the previous week
 with open("../scripts/sim_team_details.js", "w") as jsFile:
     jsFile.write("var sim_team_details = new Array();\n")
     for i in range(1, len(hisdata)):
@@ -35,34 +36,32 @@ with open("../scripts/sim_team_details.js", "w") as jsFile:
                     teams.append(cpdata[i][j+1].strip().replace(" ",""))
             line = line[:len(line)-1] +"});\n"
             jsFile.write(line)
-
-# with open("../scripts/sim_in_progress_games.js", "w") as jsFile:
-#     jsFile.write("var sim_in_progress_games = new Array();\n")
-#     for i in range(1, len(ipdata)):
-#         if (ipdata[i][0] == "2018"):
-#             line = "    sim_in_progress_games.push({"
-#             for j in range(len(header)-1):
-#                 line += header[j+1] + ":"
-#                 line += '"' + ipdata[i][j+1].strip().replace(" ","") + '",'
-#                 if (header[j+1] == "team" and ipdata[i][j+1].strip().replace(" ","") not in teams):
-#                     teams.append(ipdata[i][j+1].strip().replace(" ",""))
-#             line = line[:len(line)-1] +"});\n"
-#             jsFile.write(line)
-
-with open("../scripts/sim_in_progress_games.js", "w") as jsFile:
-    jsFile.write("var sim_in_progress_games = new Array();\n")
-    for i in range(1, len(wk13ipdata)):
-        if (wk13ipdata[i][0] == "2018"):
-            line = "    sim_in_progress_games.push({"
+    for i in range(1, len(wk12cpdata)):
+        if (wk12cpdata[i][0] == "2018"):
+            line = "    sim_team_details.push({"
             for j in range(len(header)-1):
                 line += header[j+1] + ":"
-                line += '"' + wk13ipdata[i][j+1].strip().replace(" ","") + '",'
-                if (header[j+1] == "team" and wk13ipdata[i][j+1].strip().replace(" ","") not in teams):
-                    teams.append(wk13ipdata[i][j+1].strip().replace(" ",""))
+                line += '"' + wk12cpdata[i][j+1].strip().replace(" ","") + '",'
+                if (header[j+1] == "team" and wk12cpdata[i][j+1].strip().replace(" ","") not in teams):
+                    teams.append(wk12cpdata[i][j+1].strip().replace(" ",""))
             line = line[:len(line)-1] +"});\n"
             jsFile.write(line)
 
+# data for the in-progress games.  This is used to simulate real-time data feeds
+with open("../scripts/sim_in_progress_games.js", "w") as jsFile:
+    jsFile.write("var sim_in_progress_games = new Array();\n")
+    for i in range(1, len(ipdata)):
+        if (ipdata[i][0] == "2018"):
+            line = "    sim_in_progress_games.push({"
+            for j in range(len(header)-1):
+                line += header[j+1] + ":"
+                line += '"' + ipdata[i][j+1].strip().replace(" ","") + '",'
+                if (header[j+1] == "team" and ipdata[i][j+1].strip().replace(" ","") not in teams):
+                    teams.append(ipdata[i][j+1].strip().replace(" ",""))
+            line = line[:len(line)-1] +"});\n"
+            jsFile.write(line)
 
+# generate the list of teams as found in the datasets
 with open("../scripts/teams.js", "w") as jsFile:
     jsFile.write("var teams = new Array();\n")
     for i in range(len(teams)):
